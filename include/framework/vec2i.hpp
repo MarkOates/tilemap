fba4e244b4c0b4aa5138ab65dd4350cdac1e5c96
Mark Oates <mark.oates@shopify.com>
2018-04-28 16:57:44 -0400

    Add vec2i to framework

---
+++ include/framework/vec2i.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+
+class vec2i
+{
+public:
+   int x, y;
+
+   vec2i(int x=0, int y=0);
+   ~vec2i();
+};
+
+
---
+++ src/framework/vec2i.cpp
@@ -0,0 +1,17 @@
+
+
+#include <framework/vec2i.hpp>
+
+
+vec2i::vec2i(int x, int y)
+   : x(x)
+   , y(y)
+{
+}
+
+
+vec2i::~vec2i()
+{
+}
+
+
