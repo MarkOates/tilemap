e5e437d430e0318932b56bf004096c4734081daf
Mark Oates <mark.oates@shopify.com>
2018-04-28 20:49:03 -0400

    Cleanup NextCollidingTilesCalculator, use +/- 0.00001 offset on edges

--- src/tilemap/lib/NextCollidingTilesCalculator.cpp
+++ src/tilemap/lib/NextCollidingTilesCalculator.cpp
@@ -64,48 +64,34 @@ std::vector<vec2i> NextCollidingTilesCalculator::next_colliding_tiles()
    // if moving right, test along the right edge
    if (velocity_amt > 0.0) // moving right
    {
-      now_x += size_of_body;
-      next_x += size_of_body;
+      now_x += size_of_body - 0.00001;
+      next_x += size_of_body - 0.00001;
+   }
+   else
+   {
+      now_x += 0.00001;
+      next_x += 0.00001;
    }
 
    int column_x_now = now_x / 16;
    int column_x_next = next_x / 16;
-   //std::vector<int2> collided_tiles;
-
-   if (now_x != next_x)
-   {
-      //std::cout << "---" << std::endl;
-      //std::cout << "now_x: " << now_x << std::endl;
-      //std::cout << "next_x: " << next_x << std::endl;
-      //std::cout << "column_x_now: " << column_x_now << std::endl;
-      //std::cout << "column_x_next: " << column_x_next << std::endl;
-   }
 
    float remainder_y = fmod(next_y, 16.0);
 
    if (column_x_now != column_x_next)
    {
-      //std::cout << "PASS" << " was: " << column_x_now << " now: " << column_x_next << std::endl;
-      // the player has passed from one row of tiles into the next
-      // collect the tiles along the "next" left edge
       int num_passes = std::ceil((length_of_edge + remainder_y) / 16);
       float step_cursor = 0;
 
-      //std::cout << "NUMPAS " << num_passes << std::endl;
-
       result_tiles.reserve(num_passes);
+      vec2i collided_tile;
+
       for (unsigned i=0; i<num_passes; i++)
       {
-         //if (i==(num_passes-1)) step_cursor = length_of_edge;
-
-         vec2i collided_tile(0, 0);
-
          if (axis == X_AXIS) collided_tile = world_to_tile_coordinates(next_x, next_y + step_cursor);
          else if (axis == Y_AXIS) collided_tile = world_to_tile_coordinates(next_y + step_cursor, next_x);
 
          result_tiles.push_back(collided_tile);
-
-         //std::cout << "   passed tiles:" << collided_tile << std::endl;
 
          step_cursor += cursor_step_amt;
       }
--- tests/tilemap/lib/NextCollidingTilesCalculatorTest.cpp
+++ tests/tilemap/lib/NextCollidingTilesCalculatorTest.cpp
@@ -2,18 +2,23 @@
 
 #include <gtest/gtest.h>
 
-#include <tilemap/lib/NextCollidingTilesCalculatorTest.hpp>
+#include <tilemap/lib/NextCollidingTilesCalculator.hpp>
 
 
-TEST(NextCollidingTilesCalculatorTest, can_be_created)
+TEST(NextCollidingTilesCalculatorTest, can_be_created_without_arguments)
 {
-   NextCollidingTilesCalculator calculator;
+   //NextCollidingTilesCalculator calculator;
 }
 
 
-TEST(NextCollidingTilesCalculatorTest, next_colliding_tiles__returns_a_list_of_tiles_that_are_expected_to_collide_in_the_next_step)
+TEST(NextCollidingTilesCalculatorTest, next_colliding_tiles__returns_a_list_of_tiles_that_are_expected_to_collide_in_the_next_step_along_the_x_axis)
 {
-   NextCollidingTilesCalculator calculator;
+   NextCollidingTilesCalculator calculator(NextCollidingTilesCalculator::X_AXIS, 0, 0, 16, 16, 8, 0);
+
+   std::vector<vec2i> expected_result_tiles = { { 1, 0 } };
+   std::vector<vec2i> returned_result_tiles = calculator.next_colliding_tiles();
+
+   ASSERT_EQ(expected_result_tiles, returned_result_tiles);
 }
 
 
