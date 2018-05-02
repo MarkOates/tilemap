e70e61d8d6772cc258aea95b30a90ac5b05f0bcc
Mark Oates <mark.oates@shopify.com>
2018-05-01 20:39:50 -0400

    Add additional matrix filters for 2d convolution

--- include/tilemap/lib/TileNeighborMatcher.hpp
+++ include/tilemap/lib/TileNeighborMatcher.hpp
@@ -17,6 +17,10 @@ public:
    TileNeighborMatcher();
    ~TileNeighborMatcher();
 
+   const int full[3][3] =         { { _, 1, _ },
+                                    { 1, 1, 1 },
+                                    { _, 1, _ } };
+
    const int top_left[3][3] =     { { _, 2, _ },
                                     { 2, 1, 1 },
                                     { _, 1, _ } };
@@ -49,9 +53,33 @@ public:
                                     { 1, 1, 2 },
                                     { _, 1, _ } };
 
-   const int full[3][3] =         { { _, 1, _ },
-                                    { 1, 1, 1 },
+   const int top_tip[3][3] =      { { _, 2, _ },
+                                    { 2, 1, 2 },
                                     { _, 1, _ } };
+
+   const int bottom_tip[3][3] =   { { _, 1, _ },
+                                    { 2, 1, 2 },
+                                    { _, 2, _ } };
+
+   const int left_tip[3][3] =     { { _, 2, _ },
+                                    { 2, 1, 1 },
+                                    { _, 2, _ } };
+
+   const int right_tip[3][3] =    { { _, 2, _ },
+                                    { 1, 1, 2 },
+                                    { _, 2, _ } };
+
+   const int center[3][3] =       { { _, 1, _ },
+                                    { 2, 1, 2 },
+                                    { _, 1, _ } };
+
+   const int middle[3][3] =       { { _, 2, _ },
+                                    { 1, 1, 1 },
+                                    { _, 2, _ } };
+
+   const int isolated[3][3] =     { { _, 2, _ },
+                                    { 2, 1, 2 },
+                                    { _, 2, _ } };
 
    bool match(const int truth_matrix[3][3], int test_tile, const int context_matrix[3][3]);
 };
